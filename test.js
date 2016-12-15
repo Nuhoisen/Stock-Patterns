8de3249ead1a94df1ee89a43e83ef60623779e9a
Nuhoisen <kellyhonsing@gmail.com>
2016-12-15 15:22:37 -0700

    adding stock ticker auto fill

--- test.js
+++ test.js
@@ -14,13 +14,62 @@ function httpGetAsync(theUrl, callback)
     xmlHttp.send(null);
 }
 
+
+function httpGetJSON(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.onerror = function(){
+    	alert("nope, not working");
+    }
+
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
+
+function parseJSON(response){
+	var jsonResponse = JSON.parse(response);
+	alert(jsonResponse);
+}
+
+httpGetJSON("http://chstocksearch.herokuapp.com/api/apple", parseJSON);
+
 function printJSON(response){
 	config = {};
 	var results = Papa.parse(response);
 
 	//var csvContent = "data:text/csv;carset=utf-8";
 }
+var countries = [
+   { value: 'Andorra', data: 'AD' },
+   // ...
+   { value: 'Zimbabwe', data: 'ZZ' }
+];
 
+
+var options = {
+
+  lookup: countries,
+ 
+  list: {	
+    match: {
+      enabled: true
+    }
+  },
+
+
+  theme: "square",
+	onSelect: function (suggestion) {
+	        alert('You selected: ' + suggestion.value + ', ' + suggestion.data);
+    }
+};
+$('#tickerInput').autocomplete().disable();
+$('#tickerInput').autocomplete('setOptions', options);
+	
+	
 
 function setupURL(stockSyms){
 	url = "http://finance.yahoo.com/d/quotes.csv?s="
